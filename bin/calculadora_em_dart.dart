9077c1772b51d3ad9888efc23cd0e68d2502606e
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-11-04 11:11:29 -0300

    projeto criado para conhecimento da linguagem dart, será desenvolvido uma calculadora. Inicialmente, criei a função main, armazenei em variáveis os dois números que o usuário irá digitar, já fazendo a conversão e não recebendo nulos, criei a função para cada operação e as chamo no final

---
+++ bin/calculadora_em_dart.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+
+void main() {
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync()!);
+  
+
+  void soma(){
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao(){
+    print(numeroUm - numeroDois);
+  }
+
+  void multiplicacao(){
+    print(numeroUm * numeroDois);
+  }
+
+  void divisao(){
+    print(numeroUm / numeroDois);
+  }
+
+  soma();
+  subtracao();
+  multiplicacao();
+  divisao();
+
+}
