cc68822ace8525304dbea7e62f25a6ca0da1825f
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-11-04 21:29:54 -0300

    adicionando as funções de cada operação para o cálculo, refatorando o código para evitar erros, e criando mensagens para o usuário.

--- bin/calculadora_em_dart.dart
+++ bin/calculadora_em_dart.dart
@@ -1,30 +1,71 @@
 import 'dart:io';
 
-
 void main() {
-  double numeroUm = double.parse(stdin.readLineSync()!);
-  double numeroDois = double.parse(stdin.readLineSync()!);
-  
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
 
-  void soma(){
+  void soma() {
     print(numeroUm + numeroDois);
   }
 
-  void subtracao(){
+  void subtracao() {
     print(numeroUm - numeroDois);
   }
 
-  void multiplicacao(){
+  void multiplicacao() {
     print(numeroUm * numeroDois);
   }
 
-  void divisao(){
+  void divisao() {
     print(numeroUm / numeroDois);
   }
 
-  soma();
-  subtracao();
-  multiplicacao();
-  divisao();
+  void calcular() {
+    switch (operacao) {
+      case "+":
+        soma();
+        break;
+      case "-":
+        subtracao();
+        break;
+      case "*":
+        multiplicacao();
+        break;
+      case "/":
+        divisao();
+        break;
+      default:
+        print("Opção Inválida");
+    }
+  }
 
+
+  print("Digite o primeiro valor");
+  // o simbolo ? indica que pode vir nulo
+  String? entrada = stdin.readLineSync();
+
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada);
+    }
+  }
+
+  print("Digite uma operação");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    operacao = entrada;
+  }
+
+  print("Digite o segundo valor");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada);
+    }
+  }
+  
+
+  print("O resultado da operação é:");
+  calcular();
 }
